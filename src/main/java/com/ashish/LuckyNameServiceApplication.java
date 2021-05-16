bb504bbbcff454c1e835e7a601d75e6081fcf69c
AS008BA <ashish.bankar@pb.com>
2021-05-16 16:10:57 +0530

    removing package name

--- src/main/java/com/ashish/LuckyNameServiceApplication.java
+++ src/main/java/com/ashish/LuckyNameServiceApplication.java
@@ -7,7 +7,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @SpringBootApplication
-@ComponentScan("com.clarion")
+@ComponentScan("com")
 public class LuckyNameServiceApplication {
 
 	public static void main(String[] args) {
